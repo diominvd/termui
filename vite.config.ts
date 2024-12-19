59ffe8c61b2072dfcc41d97d13494e6668fdd12a
diominvd <diominvd@HOME-PC>
2024-12-20 00:58:32 +0300

    chore: initial commit
    The TypeScript configuration and the Vite project builder are configured. Added package information to package.json

---
+++ vite.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from 'vite';
+import { extname, relative, basename } from 'path';
+import { fileURLToPath } from 'node:url';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { glob } from 'glob';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      rollupTypes: true,
+      outDir: 'build/types',
+      insertTypesEntry: true,
+      include: ['src'],
+      exclude: ['**/*.test.*']
+    }),
+    libInjectCss(),
+  ],
+  build: {
+    outDir: 'build',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'termui',
+      formats: ['es'],
+      fileName: () => `index.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob.sync('src/**/*.{ts,tsx}').map(file => [
+          file.includes('/components/') 
+            ? `components/${basename(file, extname(file))}` 
+            : relative('src', file.slice(0, file.length - extname(file).length)),
+          fileURLToPath(new URL(file, import.meta.url))
+        ])
+      ),
+      output: {
+        minifyInternalExports: true,
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+      '@types': path.resolve(__dirname, './src/types'),
+    },
+  },
+})
